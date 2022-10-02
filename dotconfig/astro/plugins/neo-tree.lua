fa5a3f022e5a148a5ad594b417af1357c6b58d5e
Matt Briggs <matt@mattbriggs.net>
2022-10-01 17:59:45 -0700

    go back to sidebar

--- dotconfig/astro/plugins/neo-tree.lua
+++ dotconfig/astro/plugins/neo-tree.lua
@@ -1,6 +1,13 @@
 return {
+	source_selector = {
+		winbar = false,
+	},
+	default_component_config = {
+		indent = {
+			padding = 1,
+		},
+	},
 	window = {
-		position = "float",
 		mappings = {
 			["/"] = "none",
 		},
