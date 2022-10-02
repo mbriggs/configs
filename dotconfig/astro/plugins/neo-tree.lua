6debba328367fa7ef5c5c5532afb3a07c2245f18
Matt Briggs <matt@mattbriggs.net>
2022-10-02 09:32:51 -0700

    updates

--- dotconfig/astro/plugins/neo-tree.lua
+++ dotconfig/astro/plugins/neo-tree.lua
@@ -1,13 +1,19 @@
 return {
+	popup_border_style = "rounded",
 	source_selector = {
 		winbar = false,
+		content_layout = nil,
+		tab_labels = nil,
 	},
-	default_component_config = {
+	default_component_configs = {
 		indent = {
 			padding = 1,
+			indent_size = 3,
 		},
 	},
 	window = {
+		width = 40,
+
 		mappings = {
 			["/"] = "none",
 		},
