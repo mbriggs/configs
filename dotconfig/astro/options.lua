11f30ab21fac3fd4ce15b55ecdecfbb469aa7642
Matt Briggs <matt@mattbriggs.net>
2022-10-03 07:10:38 -0700

    small updates

--- dotconfig/astro/options.lua
+++ dotconfig/astro/options.lua
@@ -3,4 +3,9 @@ return {
 		catppuccin_flavour = "frappe",
 		sexp_enable_insert_mode_mappings = 0,
 	},
+	opt = {
+		cmdheight = 1,
+		relativenumber = false,
+		wrap = true,
+	},
 }
--- dotconfig/astro/plugins/null-ls.lua
+++ dotconfig/astro/plugins/null-ls.lua
@@ -4,19 +4,6 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 return {
 	debug = false,
 	sources = {
@@ -27,7 +14,6 @@ return {
 		formatting.sqlformat,
 		formatting.stylua,
 		formatting.cljstyle,
-		formatting.rubocop,
 		formatting.mix,
 
 		diagnostics.shellcheck,
@@ -41,17 +27,4 @@ return {
 		code_actions.shellcheck,
 		code_actions.refactoring,
 	},
-
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					lsp_formatting(bufnr)
-				end,
-			})
-		end
-	end,
 }
