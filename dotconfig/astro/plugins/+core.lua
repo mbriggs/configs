45a1c241b8f2a1704284253d3518580107175487
Matt Briggs <matt@mattbriggs.net>
2023-05-25 22:52:48 -0700

    fix core plugin loading

--- dotconfig/astro/plugins/+core.lua
+++ dotconfig/astro/plugins/+core.lua
@@ -2,23 +2,23 @@
 
 return {
 	-- move splits around
-	{ "sindrets/winshift.nvim", name = "winshift" },
+	{ "sindrets/winshift.nvim", cmd = "WinShift", name = "winshift" },
 	-- open file
-	{ "justinmk/vim-gtfo" },
+	{ "justinmk/vim-gtfo", event = "BufReadPost" },
 	-- correct typos on file open from cli
-	{ "mong8se/actually.nvim" },
+	{ "mong8se/actually.nvim", event = "BufReadPost" },
 	-- casing aware search and replace
-	{ "lambdalisue/reword.vim" },
+	{ "lambdalisue/reword.vim", cmd = "Reword" },
 	-- reload file with sudo
-	{ "lambdalisue/suda.vim" },
+	{ "lambdalisue/suda.vim", event = "BufReadPost" },
 	-- utility mappings
-	{ "tpope/vim-unimpaired" },
+	{ "tpope/vim-unimpaired", event = "BufReadPost" },
 	-- highlight unique chars
-	{ "jinh0/eyeliner.nvim" },
+	{ "jinh0/eyeliner.nvim", event = "BufReadPost" },
 	-- zoom pane with <C-w>m
-	{ "dhruvasagar/vim-zoom" },
+	{ "dhruvasagar/vim-zoom", event = "BufReadPost" },
 	-- shadowenv integration
-	{ "Shopify/shadowenv.vim", lazy = false },
+	{ "Shopify/shadowenv.vim", event = "BufReadPost" },
 	-- copilot
 	{
 		"github/copilot.vim",
