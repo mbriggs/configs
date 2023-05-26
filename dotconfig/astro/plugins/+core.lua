ce9056ef0d89c6b54e9cb9099856de13513e0fb5
Matt Briggs <matt@mattbriggs.net>
2023-05-26 13:11:41 -0700

    spider

--- dotconfig/astro/plugins/+core.lua
+++ dotconfig/astro/plugins/+core.lua
@@ -19,4 +19,20 @@ return {
 	{ "dhruvasagar/vim-zoom", event = "BufReadPost" },
 	-- shadowenv integration
 	{ "Shopify/shadowenv.vim", event = "BufReadPost" },
+	-- spider
+	{
+		"chrisgrieser/nvim-spider",
+		event = "BufReadPost",
+		config = function()
+			vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+			vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+			vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+			vim.keymap.set(
+				{ "n", "o", "x" },
+				"ge",
+				"<cmd>lua require('spider').motion('ge')<CR>",
+				{ desc = "Spider-ge" }
+			)
+		end,
+	},
 }
