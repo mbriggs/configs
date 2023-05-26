29795d7581738a11afecae87b94c709093173288
Matt Briggs <matt@mattbriggs.net>
2023-05-26 09:52:48 -0700

    fix some small plugin issue

--- dotconfig/astro/plugins/+fix.lua
+++ dotconfig/astro/plugins/+fix.lua
@@ -5,27 +5,34 @@ return {
 		"folke/todo-comments.nvim",
 		name = "todo-comments",
 		requires = "nvim-lua/plenary.nvim",
+		event = "BufRead",
 	},
 	-- open files over ssh
-	{ "lambdalisue/vim-protocol" },
+	{ "lambdalisue/vim-protocol", lazy = false },
 	-- highlight yanks
-	{ "machakann/vim-highlightedyank" },
+	{ "machakann/vim-highlightedyank", event = "TextYankPost" },
 	-- support . for more things
-	{ "tpope/vim-repeat" },
+	{ "tpope/vim-repeat", event = "BufRead" },
 	-- work on delims
-	{ "kylechui/nvim-surround" },
+	{
+		"kylechui/nvim-surround",
+		lazy = false,
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
 	-- advanced qflist
 	{
 		"kevinhwang91/nvim-bqf",
 		requires = {
-			{ 
-			  "junegunn/fzf", 
-			  dir = "~/.fzf", 
-			  build = "./install --all" 
+			{
+				"junegunn/fzf",
+				dir = "~/.fzf",
+				build = "./install --all",
 			},
 			"junegunn/fzf.vim",
 		},
 		ft = "qf",
+		lazy = false,
 	},
-
 }
