b5284ada6666718094da5aeaa171a5bb7919b5ad
Matt Briggs <matt@mattbriggs.net>
2023-05-26 09:52:48 -0700

    fix some small plugin issue

--- dotconfig/astro/plugins/+lsp.lua
+++ dotconfig/astro/plugins/+lsp.lua
@@ -1,12 +1,6 @@
 -- LSP --
 -- language server tools
 return {
-	-- popup documentation
-	{
-		"ray-x/lsp_signature.nvim",
-		name = "lsp_signature",
-		event = "BufRead",
-	},
 	-- refactoring tools
 	{
 		"ThePrimeagen/refactoring.nvim",
@@ -24,7 +18,7 @@ return {
 	{
 		"folke/lsp-trouble.nvim",
 		name = "trouble",
-		requires = "kyazdani42/nvim-web-devicons",
+		requires = { "kyazdani42/nvim-web-devicons", lazy = false },
 		cmd = "TroubleToggle",
 	},
 	{
