defae6f0bed476e1424bd945df62c23889f20acf
Matt Briggs <matt@mattbriggs.net>
2023-06-09 20:33:07 -0700

    drop spider

--- dotconfig/astro/plugins/+core.lua
+++ dotconfig/astro/plugins/+core.lua
@@ -13,26 +13,16 @@ return {
 	{ "lambdalisue/suda.vim", event = "BufReadPost" },
 	-- utility mappings
 	{ "tpope/vim-unimpaired", event = "BufReadPost" },
-	-- highlight unique chars
-	{ "jinh0/eyeliner.nvim", event = "BufReadPost" },
 	-- zoom pane with <C-w>m
 	{ "dhruvasagar/vim-zoom", event = "BufReadPost" },
 	-- shadowenv integration
 	{ "Shopify/shadowenv.vim", event = "BufReadPost" },
 	{
 		"chrisgrieser/nvim-spider",
-		lazy = false,
-		config = function()
-			vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-			vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-			vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-			vim.keymap.set(
-				{ "n", "o", "x" },
-				"ge",
-				"<cmd>lua require('spider').motion('ge')<CR>",
-				{ desc = "Spider-ge" }
-			)
-		end,
+		name = "spider",
+		opts = {
+			skipInsignificantPunctuation = true,
+		},
 	},
 	-- editorconfig
 	{
