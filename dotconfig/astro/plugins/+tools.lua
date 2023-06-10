11c9b3e3fa2213eb00784c29c54ca204e3db9398
Matt Briggs <matt@mattbriggs.net>
2023-06-09 20:33:25 -0700

    formatting

--- dotconfig/astro/plugins/+tools.lua
+++ dotconfig/astro/plugins/+tools.lua
@@ -1,25 +1,30 @@
 -- Tools --
 -- add tools to nvim
 return {
-  -- diff stuff
-  { "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" },
+	-- diff stuff
+	{ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" },
 
-  -- run tests
-  {
-    "janko/vim-test",
-    requires = "preservim/vimux",
-    config = [[vim.g['test#strategy'] = 'vimux']],
-  },
-  -- with code coverage
-  {
-    "andythigpen/nvim-coverage",
-    name = "coverage",
-    requires = "nvim-lua/plenary.nvim",
-  },
-  -- search & replace panel
-  {
-    "windwp/nvim-spectre",
-    name = "spectre",
-    requires = "nvim-lua/plenary.nvim",
-  },
+	-- run tests
+	{
+		"janko/vim-test",
+		dependencies = {
+			"christoomey/vim-tmux-runner",
+		},
+		config = function()
+			vim.g["test#strategy"] = "vtr"
+		end,
+		cmd = { "TestFile", "TestNearest", "TestSuite", "TestLast" },
+	},
+	-- with code coverage
+	{
+		"andythigpen/nvim-coverage",
+		name = "coverage",
+		requires = "nvim-lua/plenary.nvim",
+	},
+	-- search & replace panel
+	{
+		"windwp/nvim-spectre",
+		name = "spectre",
+		requires = "nvim-lua/plenary.nvim",
+	},
 }
