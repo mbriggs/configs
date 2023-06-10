d0d1dab60d0276e1de957d331a8464054e9f534e
Matt Briggs <matt@mattbriggs.net>
2023-06-09 20:33:43 -0700

    cmp changes

--- dotconfig/astro/plugins/cmp.lua
+++ dotconfig/astro/plugins/cmp.lua
@@ -3,9 +3,21 @@ return {
 	opts = function(_, opts)
 		local cmp = require("cmp")
 
-		opts.mapping["<CR>"] = cmp.mapping.confirm({ select = true })
-		opts.mapping["<Tab>"] = cmp.config.disable
-		opts.mapping["<S-Tab>"] = cmp.config.disable
+		-- opts.mapping["<CR>"] = cmp.mapping.confirm({ select = true })
+		opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+			-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+			if cmp.visible() then
+				local entry = cmp.get_selected_entry()
+				if not entry then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					cmp.confirm()
+				end
+			else
+				fallback()
+			end
+		end, { "i", "s", "c" })
+		-- opts.mapping["<S-Tab>"] = cmp.config.disable
 		opts.mapping["<ESC>"] = function(fallback)
 			if cmp.visible() then
 				cmp.abort()
@@ -14,13 +26,24 @@ return {
 			end
 		end
 
-		cmp.event:on("menu_opened", function()
-			vim.b.copilot_suggestion_hidden = true
-		end)
+		-- cmp.event:on("menu_opened", function()
+		-- 	vim.b.copilot_suggestion_hidden = true
+		-- end)
+		--
+		-- cmp.event:on("menu_closed", function()
+		-- 	vim.b.copilot_suggestion_hidden = false
+		-- end)
 
-		cmp.event:on("menu_closed", function()
-			vim.b.copilot_suggestion_hidden = false
-		end)
+		opts.enabled = function()
+			-- disable completion in comments
+			local context = require("cmp.config.context")
+			-- keep command mode completion enabled when cursor is in a comment
+			if vim.api.nvim_get_mode().mode == "c" then
+				return true
+			else
+				return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+			end
+		end
 
 		return opts
 	end,
