f5fe5bbb0d8b700f478cb745a7f5d8b11ad5e778
Matt Briggs <matt@mattbriggs.net>
2023-06-09 20:33:56 -0700

    fix tmux

--- dotconfig/astro/plugins/tmux.lua
+++ dotconfig/astro/plugins/tmux.lua
@@ -1,19 +1,26 @@
 -- tmux integration
 
 return {
-	"aserowy/tmux.nvim",
-	name = "tmux",
-	opts = {
-		copy_sync = {
-			enable = false,
+	{
+		"aserowy/tmux.nvim",
+		name = "tmux",
+		opts = {
+			copy_sync = {
+				enable = false,
+			},
+			navigation = {
+				cycle_navigation = true,
+				enable_default_keybindings = false,
+				persist_zoom = true,
+			},
+			resize = {
+				enable_default_keybindings = false,
+			},
 		},
-		navigation = {
-			cycle_navigation = true,
-			enable_default_keybindings = false,
-			persist_zoom = true,
-		},
-		resize = {
-			enable_default_keybindings = false,
-		},
-	}
+	},
+
+	{
+		"christoomey/vim-tmux-runner",
+		cmd = "VtrSendCommandToRunner",
+	},
 }
