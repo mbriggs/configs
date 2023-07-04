3a7aeddea5c50e706ba432ba26c4b6774670c1fe
Matt Briggs <matt@mattbriggs.net>
2023-07-04 09:20:36 -0700

    configure flash

--- dotconfig/astro/plugins/flash.lua
+++ dotconfig/astro/plugins/flash.lua
@@ -3,14 +3,20 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
-	keys = {
-		{
-			"s",
-			mode = { "n", "o", "x" },
-			function()
-				require("flash").treesitter()
-			end,
+	opts = {
+		keys = {
+			{
+				"s",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+			},
+		},
+		modes = {
+			char = {
+				enabled = false,
+			},
 		},
 	},
 }
