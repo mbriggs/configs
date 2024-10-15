35c04b31a762d6a8eb7839c5b9509e64b809ffd0
Matt Briggs <matt@mattbriggs.net>
2024-10-14 21:26:41 -0400

    small fixes

--- dotconfig/nvim/lua/mbriggs/completion.lua
+++ dotconfig/nvim/lua/mbriggs/completion.lua
@@ -1,7 +1,8 @@
 local map = vim.keymap.set
 
 -- dabbrev style completion
-map("i", "<D-/>", "<C-x><C-n>", { desc = "next completion" })
+map("i", "<D-/>", "<C-n>", { desc = "next completion", noremap = "true" })
+map("i", "<D-S-/>", "<C-x><C-n>", { desc = "next completion in current file", noremap = "true" })
 
 map({ "s", "i" }, "<Tab>", function()
 	if vim.snippet.active({ direction = 1 }) then
