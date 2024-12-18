077d3407239349f4c5392cb9279d484f686b8a87
Matt Briggs <matt@mattbriggs.net>
2024-12-18 14:21:06 -0500

    improve quickfix functions

--- dotconfig/nvim/lua/mbriggs/quickfix.lua
+++ dotconfig/nvim/lua/mbriggs/quickfix.lua
@@ -26,28 +26,36 @@ end, { desc = "toggle quickfix" })
 -- removing items from quickfix
 local del_qf_item = function()
   local items = vim.fn.getqflist()
+  if #items == 0 then
+    vim.notify("Quickfix list is empty", vim.log.levels.WARN)
+    return
+  end
+
   local line = vim.fn.line(".")
   if line <= #items then
     table.remove(items, line)
+    vim.fn.setqflist(items, "r")
+    if line > #items then
+      line = #items
+    end
+    vim.api.nvim_win_set_cursor(0, { line, 0 })
   end
-  vim.fn.setqflist(items, "r")
-  if line > #items then
-    line = #items
-  end
-  vim.api.nvim_win_set_cursor(0, { line, 0 })
 end
 
 vim.api.nvim_create_autocmd("FileType", {
-  desc = "remove items from quickfix list",
+  pattern = "qf",
+  desc = "Setup quickfix list item removal",
   callback = function(opts)
-    local ft = vim.bo[opts.buf].filetype
-
-    if ft == "qf" then
-      vim.keymap.set("n", "x", function()
-        del_qf_item()
-        vim.cmd("silent! call repeat#set('x', v:count)")
-      end, { silent = true, buffer = true, desc = "Remove entry from QF" })
-    end
+    -- Normal mode mapping
+    vim.keymap.set("n", "x", function()
+      del_qf_item()
+      vim.cmd("silent! call repeat#set('x', v:count)")
+    end, {
+      silent = true,
+      buffer = opts.buf,
+      desc = "Remove entry from quickfix list",
+      nowait = true,
+    })
   end,
 })
 
