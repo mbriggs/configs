c94028e741fdadee025e27000c33169a7bdb4a84
Matt Briggs <matt@mattbriggs.net>
2024-12-20 09:02:24 -0500

    rework harpoon configs

--- dotconfig/nvim/lua/plugins/blink.cmp.lua
+++ dotconfig/nvim/lua/plugins/blink.cmp.lua
@@ -1,56 +1,56 @@
 local snippets_dir = vim.fn.stdpath("config") .. "/snippets"
 
 return {
-  "saghen/blink.cmp",
-  lazy = "VeryLazy",
-  version = "v0.*",
+	"saghen/blink.cmp",
+	lazy = "VeryLazy",
+	version = "v0.*",
 
-  dependencies = {
-    {
-      "chrisgrieser/nvim-scissors",
-      lazy = false,
-      dependencies = {
-        "ibhagwan/fzf-lua",
-        "stevearc/dressing.nvim"
-      },
-      opts = {
-        snippetDir = snippets_dir,
-      },
-      keys = {
-        {
-          "<leader>Sa",
-          [[<cmd>ScissorsAddNewSnippet<cr>]],
-          mode = { "v", "n" },
-          silent = true,
-          desc = "Add Snippet",
-        },
-        { "<leader>Se", [[<cmd>ScissorsEditSnippet<cr>]], silent = true, desc = "Add Snippet" },
-      },
-    },
-  },
+	dependencies = {
+		{
+			"chrisgrieser/nvim-scissors",
+			lazy = false,
+			dependencies = {
+				"ibhagwan/fzf-lua",
+				"stevearc/dressing.nvim",
+			},
+			opts = {
+				snippetDir = snippets_dir,
+			},
+			keys = {
+				{
+					"<leader>Sa",
+					[[<cmd>ScissorsAddNewSnippet<cr>]],
+					mode = { "v", "n" },
+					silent = true,
+					desc = "Add Snippet",
+				},
+				{ "<leader>Se", [[<cmd>ScissorsEditSnippet<cr>]], silent = true, desc = "Add Snippet" },
+			},
+		},
+	},
 
-  opts = {
-    -- experimental signature help support
-    trigger = { signature_help = { enabled = false } },
-    highlight = { use_nvim_cmp_as_default = true },
-    keymap = {
-      ["<D-space>"] = { "show" },
-      ["<S-CR>"] = { "hide" },
-      ["<C-y>"] = { "accept" },
-      ["<C-n>"] = { "select_next" },
-      ["<C-p>"] = { "select_prev" },
-      ["<PageDown>"] = { "scroll_documentation_down" },
-      ["<PageUp>"] = { "scroll_documentation_up" },
-    },
-    nerd_font_variant = "mono",
-    sources = {
-      providers = {
-        snippets = {
-          opts = {
-            search_paths = { snippets_dir },
-          },
-        },
-      },
-    },
-  },
+	opts = {
+		-- experimental signature help support
+		trigger = { signature_help = { enabled = false } },
+		highlight = { nerd_font_variant = "mono" },
+		keymap = {
+			["<D-space>"] = { "show" },
+			["<S-CR>"] = { "hide" },
+			["<C-y>"] = { "accept" },
+			["<C-n>"] = { "select_next" },
+			["<C-p>"] = { "select_prev" },
+			["<PageDown>"] = { "scroll_documentation_down" },
+			["<PageUp>"] = { "scroll_documentation_up" },
+		},
+		nerd_font_variant = "mono",
+		sources = {
+			providers = {
+				snippets = {
+					opts = {
+						search_paths = { snippets_dir },
+					},
+				},
+			},
+		},
+	},
 }
--- dotconfig/nvim/lua/plugins/harpoon.lua
+++ dotconfig/nvim/lua/plugins/harpoon.lua
@@ -1,31 +1,22 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
-	event = "VeryLazy",
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup()
-		local map = vim.keymap.set
-
-		map("n", "<leader>a", function()
-			harpoon:list():add()
-		end, { noremap = true, silent = true, desc = "Add to Harpoon" })
-
-		map("n", "<leader>h", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end, { noremap = true, silent = true, desc = "Toggle Harpoon" })
-
-		map("n", "<C-h>", function()
-			harpoon:list():select(1)
-		end)
-		map("n", "<C-j>", function()
-			harpoon:list():select(2)
-		end)
-		map("n", "<C-k>", function()
-			harpoon:list():select(3)
-		end)
-		map("n", "<C-l>", function()
-			harpoon:list():select(4)
-		end)
-	end,
+	opts = {
+		settings = {
+			save_on_toggle = true,
+		},
+	},
+	keys = {
+		{ "<leader>a", "<cmd>lua require('harpoon'):list():add()<cr>", silent = true, desc = "Add to Harpoon" },
+		{
+			"<leader>h",
+			"<cmd>lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<cr>",
+			silent = true,
+			desc = "Toggle Harpoon",
+		},
+		{ "<C-h>", "<cmd>lua require('harpoon'):list():select(1)<cr>", desc = "Harpoon to file 1", silent = true },
+		{ "<C-j>", "<cmd>lua require('harpoon'):list():select(2)<cr>", desc = "Harpoon to file 2", silent = true },
+		{ "<C-k>", "<cmd>lua require('harpoon'):list():select(3)<cr>", desc = "Harpoon to file 3", silent = true },
+		{ "<C-l>", "<cmd>lua require('harpoon'):list():select(4)<cr>", desc = "Harpoon to file 4", silent = true },
+	},
 }
