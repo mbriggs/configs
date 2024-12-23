fcb146490297986a9eec9ea034a207b76d4e4687
Matt Briggs <matt@mattbriggs.net>
2024-12-23 18:55:08 -0500

    switch ruby indentation to native instead of treesitter

--- dotconfig/nvim/lua/plugins/fzf.lua
+++ dotconfig/nvim/lua/plugins/fzf.lua
@@ -1,225 +1,225 @@
 local function toggle_find_in_dir(_, ctx)
-	local bufname = vim.api.nvim_buf_get_name(ctx.__CTX.bufnr)
-	if bufname == "" then
-		return false
-	end
+  local bufname = vim.api.nvim_buf_get_name(ctx.__CTX.bufnr)
+  if bufname == "" then
+    return false
+  end
 
-	local o = vim.deepcopy(ctx.__call_opts)
+  local o = vim.deepcopy(ctx.__call_opts)
 
-	-- Get the current cwd setting
-	local using_file_dir = o.cwd and o.cwd == vim.fn.fnamemodify(vim.api.nvim_buf_get_name(ctx.__CTX.bufnr), ":h")
+  -- Get the current cwd setting
+  local using_file_dir = o.cwd and o.cwd == vim.fn.fnamemodify(vim.api.nvim_buf_get_name(ctx.__CTX.bufnr), ":h")
 
-	if using_file_dir then
-		-- Switch back to cwd
-		o.cwd = vim.fn.getcwd()
-		o.prompt = "> "
-	else
-		-- Switch to file directory
-		local current_file = vim.api.nvim_buf_get_name(ctx.__CTX.bufnr)
-		o.cwd = vim.fn.fnamemodify(current_file, ":h")
-		local prompt = o.cwd:gsub("^" .. vim.fn.getcwd() .. "/?", "")
-		o.prompt = prompt .. "> "
-	end
-	-- Keep the buffer reference
-	o.buf = ctx.__CTX.bufnr
+  if using_file_dir then
+    -- Switch back to cwd
+    o.cwd = vim.fn.getcwd()
+    o.prompt = "> "
+  else
+    -- Switch to file directory
+    local current_file = vim.api.nvim_buf_get_name(ctx.__CTX.bufnr)
+    o.cwd = vim.fn.fnamemodify(current_file, ":h")
+    local prompt = o.cwd:gsub("^" .. vim.fn.getcwd() .. "/?", "")
+    o.prompt = prompt .. "> "
+  end
+  -- Keep the buffer reference
+  o.buf = ctx.__CTX.bufnr
 
-	-- Call files with new options
-	require("fzf-lua").files(o)
+  -- Call files with new options
+  require("fzf-lua").files(o)
 end
 
 return {
-	"ibhagwan/fzf-lua",
-	dependencies = {
-		{
-			"stevearc/aerial.nvim",
-			name = "aerial",
-			dependencies = {
-				"nvim-treesitter/nvim-treesitter",
-				"nvim-tree/nvim-web-devicons",
-			},
-			config = true,
-		},
-	},
+  "ibhagwan/fzf-lua",
+  dependencies = {
+    {
+      "stevearc/aerial.nvim",
+      name = "aerial",
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = true,
+    },
+  },
 
-	cmd = "FzfLua",
-	opts = function()
-		local config = require("fzf-lua.config")
-		local actions = require("fzf-lua.actions")
+  cmd = "FzfLua",
+  opts = function()
+    local config = require("fzf-lua.config")
+    local actions = require("fzf-lua.actions")
 
-		-- Toggle root dir / cwd
-		config.defaults.actions.files["ctrl-r"] = toggle_find_in_dir
-		config.set_action_helpstr(config.defaults.actions.files["ctrl-r"], "toggle-root-dir")
+    -- Toggle root dir / cwd
+    config.defaults.actions.files["ctrl-r"] = toggle_find_in_dir
+    config.set_action_helpstr(config.defaults.actions.files["ctrl-r"], "toggle-root-dir")
 
-		return {
-			fzf_colors = true,
-			fzf_opts = {
-				["--no-scrollbar"] = true,
-				["--info"] = "inline",
-				["--tiebreak"] = "begin",
-			},
-			defaults = {
-				formatter = "path.dirname_first",
-				headers = false,
-			},
-			keymap = {
-				fzf = {
-					["ctrl-q"] = "select-all+accept",
-				},
-			},
-			winopts = {
-				width = 0.8,
-				height = 0.8,
-				row = 0.5,
-				col = 0.5,
-				preview = {
-					scrollchars = { "┃", "" },
-				},
-			},
-			files = {
-				cwd_prompt = false,
-				actions = {
-					["alt-i"] = { actions.toggle_ignore },
-					["alt-h"] = { actions.toggle_hidden },
-				},
-			},
-			grep = {
-				rg_glob = true, -- enable glob parsing
-				glob_flag = "--iglob", -- case insensitive globs
-				glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
-				actions = {
-					["alt-i"] = { actions.toggle_ignore },
-					["alt-h"] = { actions.toggle_hidden },
-				},
-			},
-			oldfiles = {
-				include_current_session = true,
-			},
-			lsp = {
-				symbols = {
-					symbol_hl = function(s)
-						return "TroubleIcon" .. s
-					end,
-					symbol_fmt = function(s)
-						return s:lower() .. "\t"
-					end,
-					child_prefix = false,
-				},
-				code_actions = {
-					previewer = vim.fn.executable("delta") == 1 and "codeaction_native" or nil,
-				},
-			},
-		}
-	end,
+    return {
+      fzf_colors = true,
+      fzf_opts = {
+        ["--no-scrollbar"] = true,
+        ["--info"] = "inline",
+        ["--tiebreak"] = "begin",
+      },
+      defaults = {
+        formatter = "path.dirname_first",
+        headers = false,
+      },
+      keymap = {
+        fzf = {
+          ["ctrl-q"] = "select-all+accept",
+        },
+      },
+      winopts = {
+        width = 0.8,
+        height = 0.8,
+        row = 0.5,
+        col = 0.5,
+        preview = {
+          scrollchars = { "┃", "" },
+        },
+      },
+      files = {
+        cwd_prompt = false,
+        actions = {
+          ["alt-i"] = { actions.toggle_ignore },
+          ["alt-h"] = { actions.toggle_hidden },
+        },
+      },
+      grep = {
+        rg_glob = true,        -- enable glob parsing
+        glob_flag = "--iglob", -- case insensitive globs
+        glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
+        actions = {
+          ["alt-i"] = { actions.toggle_ignore },
+          ["alt-h"] = { actions.toggle_hidden },
+        },
+      },
+      oldfiles = {
+        include_current_session = true,
+      },
+      lsp = {
+        symbols = {
+          symbol_hl = function(s)
+            return "TroubleIcon" .. s
+          end,
+          symbol_fmt = function(s)
+            return s:lower() .. "\t"
+          end,
+          child_prefix = false,
+        },
+        code_actions = {
+          previewer = vim.fn.executable("delta") == 1 and "codeaction_native" or nil,
+        },
+      },
+    }
+  end,
 
-	config = function(_, opts)
-		require("mbriggs.aerial-fzf") -- custom plugin for integrating with aerial
-		require("mbriggs.dir-fzf") -- custom plugin for fuzzy searching directories
+  config = function(_, opts)
+    require("mbriggs.aerial-fzf") -- custom plugin for integrating with aerial
+    require("mbriggs.dir-fzf")  -- custom plugin for fuzzy searching directories
 
-		local fzf = require("fzf-lua")
+    local fzf = require("fzf-lua")
 
-		fzf.setup(opts)
-		fzf.register_ui_select()
-	end,
-	keys = {
-		-- general
-		{ "<leader>,", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffer" },
-		{
-			"<leader>/",
-			"<cmd>FzfLua live_grep<cr>",
-			desc = "Grep",
-			mode = { "n" },
-		},
-		{
-			"<leader>/",
-			"<cmd>FzfLua grep_visual<cr>",
-			desc = "Grep",
-			mode = { "v" },
-		},
-		{ "<leader>:", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
-		{ "<leader>;", "<cmd>FzfLua files<cr>", desc = "Find Files" },
-		{ "<leader>=", "<cmd>AerialFzf<cr>", desc = "Find Doc Symbols" },
-		{ "<leader><tab>", "<cmd>FzfLua resume<cr>", desc = "FZF resume" },
+    fzf.setup(opts)
+    fzf.register_ui_select()
+  end,
+  keys = {
+    -- general
+    { "<leader>,",     "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffer" },
+    {
+      "<leader>/",
+      "<cmd>FzfLua live_grep<cr>",
+      desc = "Grep",
+      mode = { "n" },
+    },
+    {
+      "<leader>/",
+      "<cmd>FzfLua grep_visual<cr>",
+      desc = "Grep",
+      mode = { "v" },
+    },
+    { "<leader>:",     "<cmd>FzfLua command_history<cr>",                          desc = "Command History" },
+    { "<leader>;",     "<cmd>FzfLua files<cr>",                                    desc = "Find Files" },
+    { "<leader>=",     "<cmd>AerialFzf<cr>",                                       desc = "Find Doc Symbols" },
+    { "<leader><tab>", "<cmd>FzfLua resume<cr>",                                   desc = "FZF resume" },
 
-		-- find
-		{ "<leader>fd", "<cmd>DirectoryFzf<cr>", desc = "Search Directory" },
-		{ "<leader>fq", "<cmd>FzfLua quickfix<cr>", desc = "Quickfix List" },
-		{ "<leader>fj", "<cmd>FzfLua jumps<cr>", desc = "Jumplist" },
-		{ "<leader>fc", "<cmd>FzfLua commands<cr>", desc = "Command Pallet" },
-		{ "<leader>fk", "<cmd>FzfLua keymaps<cr>", desc = "Key Maps" },
-		{ "<leader>fh", "<cmd>FzfLua help_tags<cr>", desc = "Help Pages" },
-		{ "<leader>fm", "<cmd>FzfLua marks<cr>", desc = "Jump to Mark" },
+    -- find
+    { "<leader>fd",    "<cmd>DirectoryFzf<cr>",                                    desc = "Search Directory" },
+    { "<leader>fq",    "<cmd>FzfLua quickfix<cr>",                                 desc = "Quickfix List" },
+    { "<leader>fj",    "<cmd>FzfLua jumps<cr>",                                    desc = "Jumplist" },
+    { "<leader>fc",    "<cmd>FzfLua commands<cr>",                                 desc = "Command Pallet" },
+    { "<leader>fk",    "<cmd>FzfLua keymaps<cr>",                                  desc = "Key Maps" },
+    { "<leader>fh",    "<cmd>FzfLua help_tags<cr>",                                desc = "Help Pages" },
+    { "<leader>fm",    "<cmd>FzfLua marks<cr>",                                    desc = "Jump to Mark" },
 
-		-- search
-		{ "<leader>ss", "<cmd>FzfLua grep<cr>", desc = "Search" },
-		{ "<leader>sb", "<cmd>FzfLua lgrep_curbuf<cr>", desc = "Search Current Buffer" },
-		{ "<leader>sw", "<cmd>FzfLua grep_cword<cr>", desc = "Search word at cursor" },
-		{ "<leader>sw", "<cmd>FzfLua grep_cWORD<cr>", desc = "Search WORD at cursor" },
-		{ "<leader>sq", "<cmd>FzfLua lgrep_quickfix<cr>", desc = "Search quickfix" },
+    -- search
+    { "<leader>ss",    "<cmd>FzfLua grep<cr>",                                     desc = "Search" },
+    { "<leader>sb",    "<cmd>FzfLua lgrep_curbuf<cr>",                             desc = "Search Current Buffer" },
+    { "<leader>sw",    "<cmd>FzfLua grep_cword<cr>",                               desc = "Search word at cursor" },
+    { "<leader>sW",    "<cmd>FzfLua grep_cWORD<cr>",                               desc = "Search WORD at cursor" },
+    { "<leader>sq",    "<cmd>FzfLua lgrep_quickfix<cr>",                           desc = "Search quickfix" },
 
-		-- diagnostics
-		{ "<leader>x;", "<cmd>FzfLua diagnostics_document<cr>", desc = "Document Diagnostics" },
-		{ "<leader>x:", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Workspace Diagnostics" },
+    -- diagnostics
+    { "<leader>x;",    "<cmd>FzfLua diagnostics_document<cr>",                     desc = "Document Diagnostics" },
+    { "<leader>x:",    "<cmd>FzfLua diagnostics_workspace<cr>",                    desc = "Workspace Diagnostics" },
 
-		-- rails
-		{
-			"<leader>rm",
-			[[<cmd>lua require("mbriggs.rails").find_models()<cr>]],
-			silent = true,
-			desc = "Find Model",
-		},
-		{
-			"<leader>rc",
-			[[<cmd>lua require("mbriggs.rails").find_controllers()<cr>]],
-			silent = true,
-			desc = "Find Controller",
-		},
-		{
-			"<leader>rv",
-			[[<cmd>lua require("mbriggs.rails").find_views()<cr>]],
-			silent = true,
-			desc = "Find View",
-		},
-		{
-			"<leader>rh",
-			[[<cmd>lua require("mbriggs.rails").find_helpers()<cr>]],
-			silent = true,
-			desc = "Find Helper",
-		},
-		{
-			"<leader>rM",
-			[[<cmd>lua require("mbriggs.rails").find_mailers()<cr>]],
-			silent = true,
-			desc = "Find Mailer",
-		},
-		{
-			"<leader>rj",
-			[[<cmd>lua require("mbriggs.rails").find_jobs()<cr>]],
-			silent = true,
-			desc = "Find Job",
-		},
-		{
-			"<leader>rl",
-			[[<cmd>lua require("mbriggs.rails").find_lib()<cr>]],
-			silent = true,
-			desc = "Find Lib",
-		},
-		{
-			"<leader>rt",
-			[[<cmd>lua require("mbriggs.rails").find_tests()<cr>]],
-			silent = true,
-			desc = "Find Test",
-		},
-		{
-			"<leader>rs",
-			[[<cmd>lua require("mbriggs.rails").find_specs()<cr>]],
-			silent = true,
-			desc = "Find Model",
-		},
-		{
-			"<leader>rr",
-			[[<cmd>lua require("mbriggs.rails").find_related()<cr>]],
-			silent = true,
-			desc = "Find Related",
-		},
-	},
+    -- rails
+    {
+      "<leader>rm",
+      [[<cmd>lua require("mbriggs.rails").find_models()<cr>]],
+      silent = true,
+      desc = "Find Model",
+    },
+    {
+      "<leader>rc",
+      [[<cmd>lua require("mbriggs.rails").find_controllers()<cr>]],
+      silent = true,
+      desc = "Find Controller",
+    },
+    {
+      "<leader>rv",
+      [[<cmd>lua require("mbriggs.rails").find_views()<cr>]],
+      silent = true,
+      desc = "Find View",
+    },
+    {
+      "<leader>rh",
+      [[<cmd>lua require("mbriggs.rails").find_helpers()<cr>]],
+      silent = true,
+      desc = "Find Helper",
+    },
+    {
+      "<leader>rM",
+      [[<cmd>lua require("mbriggs.rails").find_mailers()<cr>]],
+      silent = true,
+      desc = "Find Mailer",
+    },
+    {
+      "<leader>rj",
+      [[<cmd>lua require("mbriggs.rails").find_jobs()<cr>]],
+      silent = true,
+      desc = "Find Job",
+    },
+    {
+      "<leader>rl",
+      [[<cmd>lua require("mbriggs.rails").find_lib()<cr>]],
+      silent = true,
+      desc = "Find Lib",
+    },
+    {
+      "<leader>rt",
+      [[<cmd>lua require("mbriggs.rails").find_tests()<cr>]],
+      silent = true,
+      desc = "Find Test",
+    },
+    {
+      "<leader>rs",
+      [[<cmd>lua require("mbriggs.rails").find_specs()<cr>]],
+      silent = true,
+      desc = "Find Model",
+    },
+    {
+      "<leader>rr",
+      [[<cmd>lua require("mbriggs.rails").find_related()<cr>]],
+      silent = true,
+      desc = "Find Related",
+    },
+  },
 }
--- dotconfig/nvim/lua/plugins/treesitter.lua
+++ dotconfig/nvim/lua/plugins/treesitter.lua
@@ -1,70 +1,84 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		build = ":TSUpdate",
-		event = { "VeryLazy" },
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all"
-				ensure_installed = {
-					"vimdoc",
-					"javascript",
-					"typescript",
-					"markdown",
-					"markdown_inline",
-					"html",
-					"embedded_template",
-					"c",
-					"lua",
-					"templ",
-					"rust",
-					"jsdoc",
-					"bash",
-					"sql",
-					"ruby",
-					"go",
-					"gomod",
-					"gowork",
-					"gosum",
-				},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+    event = { "VeryLazy" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = {
+          "vimdoc",
+          "javascript",
+          "typescript",
+          "markdown",
+          "markdown_inline",
+          "html",
+          "embedded_template",
+          "c",
+          "lua",
+          "templ",
+          "rust",
+          "jsdoc",
+          "bash",
+          "sql",
+          "ruby",
+          "go",
+          "gomod",
+          "gowork",
+          "gosum",
+        },
 
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-				auto_install = true,
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+        auto_install = true,
 
-				indent = {
-					enable = true,
-				},
+        indent = {
+          enable = true,
+          disable = { "ruby" }
+        },
 
-				highlight = {
-					-- `false` will disable the whole extension
-					enable = true,
-					additional_vim_regex_highlighting = { "markdown" },
-				},
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
+          additional_vim_regex_highlighting = { "markdown" },
+        },
 
-				autotag = {
-					enable = true,
-				},
+        autotag = {
+          enable = true,
+        },
 
-				textobjects = {
-					enable = true,
-					lookahead = true,
-				},
-			})
+        textobjects = {
+          enable = true,
+          lookahead = true,
+        },
+      })
 
-			vim.g.ruby_indent_assignment_style = "hanging"
-			vim.g.ruby_indent_block_style = "do"
-			vim.g.ruby_indent_access_modifier_style = "indent" -- Properly indents private/protected
-			vim.g.ruby_indent_hanging_elements = 1 -- Better handling of hanging elements
-			vim.g.ruby_operators = 1 -- Better operator highlighting
+      -- trying to get ts ruby stuff better
+      -- vim.g.ruby_indent_assignment_style = "variable"
+      -- vim.g.ruby_indent_block_style = "do"
+      -- vim.g.ruby_indent_access_modifier_style = "indent"
+      -- vim.g.ruby_indent_hanging_elements = 1
+      -- vim.g.ruby_operators = 1
+      -- vim.g.ruby_indent_dot_match_mark_style = 'lambda'
+      -- vim.g.ruby_indent_continuations = 1
 
-			require("nvim-treesitter.install").compilers = { "gcc-14" }
-		end,
-	},
+      -- use native ruby indentation
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "ruby",
+        callback = function()
+          vim.opt_local.indentexpr = "GetRubyIndent()"
+          vim.opt_local.autoindent = true
+          vim.opt_local.smartindent = true
+        end,
+      })
+
+      require("nvim-treesitter.install").compilers = { "gcc-14" }
+    end,
+  },
 }
