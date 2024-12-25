4d5a53ca7ff30ee9bfeac141e3ceb39cb356f75e
Matt Briggs <matt@mattbriggs.net>
2024-12-24 23:22:03 -0500

    small stuff

--- dotconfig/nvim/lua/plugins/substitute.lua
+++ dotconfig/nvim/lua/plugins/substitute.lua
@@ -1,19 +1,11 @@
 -- Substitute operator to replace text with whatever is in the unnamed register
-
 return {
 	"gbprod/substitute.nvim",
-	lazy = false,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+	config = true,
+	keys = {
+		{ "s", "<cmd>lua require('substitute').operator()<cr>", desc = "substitute" },
+		{ "ss", "<cmd>lua require('substitute').line()<cr>", desc = "substitute line" },
+		{ "S", "<cmd>lua require('substitute').eol()<cr>", desc = "substitute to eol" },
+		{ "s", "<cmd>lua require('substitute').visual()<cr>", desc = "substitute", mode = { "v", "x" } },
 	},
-	config = function(opts)
-		require("substitute").setup(opts)
-
-		vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
-		vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
-		vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
-		vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
-	end,
 }
