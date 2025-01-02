057dfaaea2a2e32e883555f5d350410b3735a70e
Matt Briggs <matt@mattbriggs.net>
2025-01-02 16:46:15 -0500

    allow c-a in insert to not be codecompanion

--- dotconfig/nvim/lua/plugins/codecompanion.lua
+++ dotconfig/nvim/lua/plugins/codecompanion.lua
@@ -1,39 +1,39 @@
 return {
-	"olimorris/codecompanion.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		{ "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
-	},
-	cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
-	opts = {
-		strategies = {
-			chat = {
-				adapter = "anthropic",
-			},
-			inline = {
-				adapter = "anthropic",
-			},
-		},
-		adapters = {
-			anthropic = function()
-				return require("codecompanion.adapters").extend("anthropic", {
-					env = {
-						api_key = "cmd:op read op://Private/Anthropic/credential --no-newline",
-					},
-				})
-			end,
-		},
-	},
-	keys = {
-		{ "<leader>cc", "<cmd>CodeCompanionChat toggle<cr>", desc = "Code Chat", mode = { "v", "n" } },
-		{
-			"<C-a>",
-			"<cmd>CodeCompanionActions<cr>",
-			silent = true,
-			desc = "Code Companion Actions",
-			mode = { "v", "n", "i" },
-		},
-		{ "ga", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add to chat", noremap = true, silent = true },
-	},
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
+  },
+  cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "anthropic",
+      },
+      inline = {
+        adapter = "anthropic",
+      },
+    },
+    adapters = {
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+          env = {
+            api_key = "cmd:op read op://Private/Anthropic/credential --no-newline",
+          },
+        })
+      end,
+    },
+  },
+  keys = {
+    { "<leader>cc", "<cmd>CodeCompanionChat toggle<cr>", desc = "Code Chat", mode = { "v", "n" } },
+    {
+      "<C-a>",
+      "<cmd>CodeCompanionActions<cr>",
+      silent = true,
+      desc = "Code Companion Actions",
+      mode = { "v", "n" },
+    },
+    { "ga",         "<cmd>CodeCompanionChat Add<cr>",    mode = "v",         desc = "Add to chat", noremap = true, silent = true },
+  },
 }
