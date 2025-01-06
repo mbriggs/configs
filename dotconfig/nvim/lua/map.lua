cdea7f81ad892257013474985cf8a5867e3df7bc
Matt Briggs <matt@mattbriggs.net>
2025-01-05 22:48:31 -0500

    small stuff

--- dotconfig/nvim/lua/map.lua
+++ dotconfig/nvim/lua/map.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.desc(t)
-  return function(desc)
-    local result = vim.deepcopy(t)
-    result["desc"] = desc
-    return result
-  end
+	return function(desc)
+		local result = vim.deepcopy(t)
+		result["desc"] = desc
+		return result
+	end
 end
 
 return M
