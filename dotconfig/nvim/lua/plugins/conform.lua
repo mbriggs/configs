0f6cbaac4373ceb9d1d8c4438ec518395460df5b
Matt Briggs <matt@mattbriggs.net>
2025-01-15 09:18:18 -0500

    use prettier_d_slim if available

--- dotconfig/nvim/lua/plugins/conform.lua
+++ dotconfig/nvim/lua/plugins/conform.lua
@@ -1,55 +1,56 @@
-local prettierconf = { "prettierd", "prettier", stop_after_first = true }
+local prettierconf = { "prettier_d_slim", "prettierd", "prettier", stop_after_first = true }
 
 return {
-	"stevearc/conform.nvim",
-	lazy = true,
-	cmd = "ConformInfo",
-	keys = {
-		{
-			"<leader>cM",
-			function()
-				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-			end,
-			mode = { "n", "v" },
-			desc = "Format Injected Langs",
-		},
-		{
-			"<leader>cm",
-			function()
-				require("conform").format()
-			end,
-			mode = { "n", "v" },
-			desc = "Format",
-		},
-	},
-	opts = function()
-		local opts = {
-			default_format_opts = {
-				timeout_ms = 3000,
-				async = false,
-				quiet = false,
-				lsp_format = "fallback",
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-				fish = { "fish_indent" },
-				sh = { "shfmt" },
-				go = { "goimports" },
-				typescript = prettierconf,
-				javascript = prettierconf,
-				typescriptreact = prettierconf,
-				html = prettierconf,
-				json = prettierconf,
-				css = prettierconf,
-				templ = { "templ" },
-				sql = { "sql-formatter" },
-				-- ruby = { "rubocop" },
-			},
+  "stevearc/conform.nvim",
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cM",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+    {
+      "<leader>cm",
+      function()
+        require("conform").format()
+      end,
+      mode = { "n", "v" },
+      desc = "Format",
+    },
+  },
+  opts = function()
+    local opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false,
+        quiet = false,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        go = { "goimports" },
+        typescript = prettierconf,
+        javascript = prettierconf,
+        typescriptreact = prettierconf,
+        markdown = prettierconf,
+        html = prettierconf,
+        json = prettierconf,
+        css = prettierconf,
+        templ = { "templ" },
+        sql = { "sql-formatter" },
+        -- ruby = { "rubocop" },
+      },
 
-			formatters = {
-				injected = { options = { ignore_errors = true } },
-			},
-		}
-		return opts
-	end,
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+      },
+    }
+    return opts
+  end,
 }
