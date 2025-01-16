363abe572e3b055907a9ab8a55032504e8091bd4
Matt Briggs <matt@mattbriggs.net>
2025-01-16 09:38:20 -0500

    moving to snacks picker from fzf

---
+++ dotconfig/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/aerial.nvim",
+  name = "aerial",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = true,
+  lazy = true,
+}
