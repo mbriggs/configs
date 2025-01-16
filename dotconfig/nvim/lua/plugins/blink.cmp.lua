f1eaf28a788d66f16f7e5c0f6844722a02a97737
Matt Briggs <matt@mattbriggs.net>
2025-01-16 09:57:11 -0500

    set up lazydev

--- dotconfig/nvim/lua/plugins/blink.cmp.lua
+++ dotconfig/nvim/lua/plugins/blink.cmp.lua
@@ -1,28 +1,31 @@
-local snippets_dir = vim.fn.stdpath("config") .. "/snippets"
-
 return {
-	"saghen/blink.cmp",
-	lazy = false,
-	version = "v0.*",
-	opts = {
-		keymap = {
-			["<D-space>"] = { "show" },
-			["<S-CR>"] = { "hide" },
-			["<C-y>"] = { "accept" },
-			["<C-n>"] = { "select_next" },
-			["<C-p>"] = { "select_prev" },
-			["<PageDown>"] = { "scroll_documentation_down" },
-			["<PageUp>"] = { "scroll_documentation_up" },
-		},
-		-- sources = {
-		--   default = { "codecompanion" },
-		--   providers = {
-		--     codecompanion = {
-		--       name = "CodeCompanion",
-		--       module = "codecompanion.providers.completion.blink",
-		--       enabled = true,
-		--     },
-		--   },
-		-- },
-	},
+  "saghen/blink.cmp",
+  lazy = false,
+  version = "v0.*",
+  opts = {
+    keymap = {
+      ["<D-space>"] = { "show" },
+      ["<S-CR>"] = { "hide" },
+      ["<C-y>"] = { "accept" },
+      ["<C-n>"] = { "select_next" },
+      ["<C-p>"] = { "select_prev" },
+      ["<PageDown>"] = { "scroll_documentation_down" },
+      ["<PageUp>"] = { "scroll_documentation_up" },
+    },
+    sources = {
+      default = { "codecompanion", "lazydev", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        codecompanion = {
+          name = "CodeCompanion",
+          module = "codecompanion.providers.completion.blink",
+          enabled = true,
+        },
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        },
+      },
+    },
+  },
 }
