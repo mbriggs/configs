d4a05bef841d5b8fbcdebc3879f463c7c3273b43
Matt Briggs <matt@mattbriggs.net>
2025-01-24 10:02:52 -0500

    switch splits in term mode

--- dotconfig/nvim/lua/plugins/kitty-navigator.lua
+++ dotconfig/nvim/lua/plugins/kitty-navigator.lua
@@ -1,22 +1,22 @@
 return {
-	"knubie/vim-kitty-navigator",
-	event = "VeryLazy",
-	build = "cp ./*.py ~/.config/kitty/",
-	init = function()
-		vim.g.kitty_navigator_no_mappings = 1
-	end,
-	config = function()
-		local map = vim.keymap.set
-		local modes = { "n", "v" }
+  "knubie/vim-kitty-navigator",
+  event = "VeryLazy",
+  build = "cp ./*.py ~/.config/kitty/",
+  init = function()
+    vim.g.kitty_navigator_no_mappings = 1
+  end,
+  config = function()
+    local map = vim.keymap.set
+    local modes = { "n", "v", "t" }
 
-		map(modes, "<D-h>", ":KittyNavigateLeft<cr>", { desc = "kitty navigate left", silent = true })
-		map(modes, "<D-j>", ":KittyNavigateDown<cr>", { desc = "kitty navigate down", silent = true })
-		map(modes, "<D-k>", ":KittyNavigateUp<cr>", { desc = "kitty navigate up", silent = true })
-		map(modes, "<D-l>", ":KittyNavigateRight<cr>", { desc = "kitty navigate right", silent = true })
+    map(modes, "<D-h>", ":KittyNavigateLeft<cr>", { desc = "kitty navigate left", silent = true })
+    map(modes, "<D-j>", ":KittyNavigateDown<cr>", { desc = "kitty navigate down", silent = true })
+    map(modes, "<D-k>", ":KittyNavigateUp<cr>", { desc = "kitty navigate up", silent = true })
+    map(modes, "<D-l>", ":KittyNavigateRight<cr>", { desc = "kitty navigate right", silent = true })
 
-		map("i", "<D-h>", "<c-o>:KittyNavigateLeft<cr>", { desc = "kitty navigate left", silent = true })
-		map("i", "<D-j>", "<c-o>:KittyNavigateDown<cr>", { desc = "kitty navigate down", silent = true })
-		map("i", "<D-k>", "<c-o>:KittyNavigateUp<cr>", { desc = "kitty navigate up", silent = true })
-		map("i", "<D-l>", "<c-o>:KittyNavigateRight<cr>", { desc = "kitty navigate right", silent = true })
-	end,
+    map("i", "<D-h>", "<c-o>:KittyNavigateLeft<cr>", { desc = "kitty navigate left", silent = true })
+    map("i", "<D-j>", "<c-o>:KittyNavigateDown<cr>", { desc = "kitty navigate down", silent = true })
+    map("i", "<D-k>", "<c-o>:KittyNavigateUp<cr>", { desc = "kitty navigate up", silent = true })
+    map("i", "<D-l>", "<c-o>:KittyNavigateRight<cr>", { desc = "kitty navigate right", silent = true })
+  end,
 }
