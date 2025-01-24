2532917949cda770782cd53605ad08b386019a36
Matt Briggs <matt@mattbriggs.net>
2025-01-24 10:02:39 -0500

    improve term configs

--- dotconfig/nvim/lua/mbriggs/term.lua
+++ dotconfig/nvim/lua/mbriggs/term.lua
@@ -1,5 +1,7 @@
 local map = vim.keymap.set
 
+vim.g.terminal_scrollback_buffer_size = 500000
+
 -- Only applies in terminal mode
 map("t", "<Esc>", "<C-\\><C-n>", { silent = true })
 
@@ -8,39 +10,39 @@ map("t", "<C-[>", "<C-\\><C-n>", { silent = true })
 
 -- Disable line numbers in terminal
 vim.api.nvim_create_autocmd("TermOpen", {
-	callback = function()
-		vim.opt_local.number = false
-		vim.opt_local.relativenumber = false
-		vim.cmd("startinsert") -- Start in insert mode
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.cmd("startinsert") -- Start in insert mode
 
-		map("t", "<D-,>", function()
-			vim.cmd("stopinsert")
-			vim.cmd("TermRename")
-		end, { buffer = true })
-	end,
+    map({ "t", "n" }, "<D-,>", function()
+      vim.cmd("stopinsert")
+      vim.cmd("TermRename")
+    end, { buffer = true })
+  end,
 })
 
 vim.api.nvim_create_autocmd("TermEnter", {
-	callback = function()
-		vim.opt.guicursor = "a:ver25" -- Vertical bar in terminal mode
-	end,
+  callback = function()
+    vim.opt.guicursor = "a:ver25" -- Vertical bar in terminal mode
+  end,
 })
 
 vim.api.nvim_create_autocmd("TermLeave", {
-	callback = function()
-		vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20" -- Reset to normal cursor
-	end,
+  callback = function()
+    vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20" -- Reset to normal cursor
+  end,
 })
 
 vim.api.nvim_create_user_command("TermRename", function()
-	vim.ui.input({
-		prompt = "Terminal name: ",
-	}, function(name)
-		if name then
-			-- Get current buffer number
-			local bufnr = vim.api.nvim_get_current_buf()
-			-- Set the buffer name
-			vim.api.nvim_buf_set_name(bufnr, name)
-		end
-	end)
+  vim.ui.input({
+    prompt = "Terminal name: ",
+  }, function(name)
+    if name then
+      -- Get current buffer number
+      local bufnr = vim.api.nvim_get_current_buf()
+      -- Set the buffer name
+      vim.api.nvim_buf_set_name(bufnr, name)
+    end
+  end)
 end, {})
