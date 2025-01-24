e7dea08b1cdbfea8961f66ee6ddaea94e03cf0c2
Matt Briggs <matt@mattbriggs.net>
2025-01-23 21:24:18 -0500

    use git signs that allow for more linespace

--- dotconfig/nvim/lua/plugins/gitsigns.lua
+++ dotconfig/nvim/lua/plugins/gitsigns.lua
@@ -1,21 +1,24 @@
+local bar = "│"
+local arrow = ""
+
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "VeryLazy",
 	opts = {
 		signs = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "" },
-			topdelete = { text = "" },
-			changedelete = { text = "▎" },
-			untracked = { text = "▎" },
+			add = { text = bar },
+			change = { text = bar },
+			delete = { text = arrow },
+			topdelete = { text = arrow },
+			changedelete = { text = bar },
+			untracked = { text = bar },
 		},
 		signs_staged = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "" },
-			topdelete = { text = "" },
-			changedelete = { text = "▎" },
+			add = { text = bar },
+			change = { text = bar },
+			delete = { text = arrow },
+			topdelete = { text = arrow },
+			changedelete = { text = bar },
 		},
 		on_attach = function(buffer)
 			local gs = package.loaded.gitsigns
