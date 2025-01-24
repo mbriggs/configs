df9d0165241cc5f3102aaa375a41277fa8391210
Matt Briggs <matt@mattbriggs.net>
2025-01-24 10:01:48 -0500

    neovide starts in home dir

--- dotconfig/nvim/lua/mbriggs/neovide.lua
+++ dotconfig/nvim/lua/mbriggs/neovide.lua
@@ -3,8 +3,8 @@ vim.opt.linespace = 6
 -- emulate stuff i set in kitty
 
 -- System clipboard paste (Cmd+V)
-vim.keymap.set("n", "<D-v>", '"+p') -- Normal mode
-vim.keymap.set("v", "<D-v>", '"+p') -- Visual mode
+vim.keymap.set("n", "<D-v>", '"+p')    -- Normal mode
+vim.keymap.set("v", "<D-v>", '"+p')    -- Visual mode
 vim.keymap.set("i", "<D-v>", "<C-r>+") -- Insert mode
 
 -- Map Cmd+F to % (jump to matching bracket)
@@ -13,7 +13,7 @@ vim.keymap.set("c", "<D-f>", '<C-r>="%"<CR>') -- Command mode for :%s/foo etc.
 
 -- Text rendering
 vim.g.neovide_text_contrast = 0.8 -- Crisper text contrast
-vim.g.neovide_text_gamma = 0.8 -- Slightly darker text rendering
+vim.g.neovide_text_gamma = 0.8    -- Slightly darker text rendering
 vim.o.guifont = "JetBrains Mono:h15:#h-slight:#e-subpixelantialias"
 
 -- Animation feel
@@ -25,3 +25,6 @@ vim.g.neovide_cursor_vfx_mode = ""
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_no_idle = false
 vim.g.neovide_cursor_antialiasing = true
+
+-- Start in home dir
+vim.cmd("cd ~")
