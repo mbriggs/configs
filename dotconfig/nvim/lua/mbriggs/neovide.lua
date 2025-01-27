579da776c11e28f6b76a2328a41e6d08e8775af8
Matt Briggs <matt@mattbriggs.net>
2025-01-26 22:57:36 -0500

    fix various neovide stuff

--- dotconfig/nvim/lua/mbriggs/neovide.lua
+++ dotconfig/nvim/lua/mbriggs/neovide.lua
@@ -3,9 +3,9 @@ vim.opt.linespace = 6
 -- emulate stuff i set in kitty
 
 -- System clipboard paste (Cmd+V)
-vim.keymap.set("n", "<D-v>", '"+p')             -- Normal mode
-vim.keymap.set("v", "<D-v>", '"+p')             -- Visual mode
-vim.keymap.set("i", "<D-v>", "<C-r>+")          -- Insert mode
+vim.keymap.set("n", "<D-v>", '"+p') -- Normal mode
+vim.keymap.set("v", "<D-v>", '"+p') -- Visual mode
+vim.keymap.set("i", "<D-v>", "<C-r>+") -- Insert mode
 vim.keymap.set("t", "<D-v>", '<C-\\><C-n>"+pi') -- Terminal mode
 
 -- Map Cmd+F to % (jump to matching bracket)
@@ -14,7 +14,7 @@ vim.keymap.set("c", "<D-f>", '<C-r>="%"<CR>') -- Command mode for :%s/foo etc.
 
 -- Text rendering
 vim.g.neovide_text_contrast = 0.8 -- Crisper text contrast
-vim.g.neovide_text_gamma = 0.8    -- Slightly darker text rendering
+vim.g.neovide_text_gamma = 0.8 -- Slightly darker text rendering
 vim.o.guifont = "JetBrainsMono Nerd Font:h15:#h-slight:#e-subpixelantialias"
 
 -- Animation feel
@@ -26,6 +26,14 @@ vim.g.neovide_cursor_vfx_mode = ""
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_no_idle = false
 vim.g.neovide_cursor_antialiasing = true
+
+-- needed to be going off of the right ruby when project changes
+vim.api.nvim_create_autocmd("DirChanged", {
+	pattern = "*",
+	callback = function()
+		vim.fn.system('eval "$(rbenv init - zsh)"')
+	end,
+})
 
 -- Start in home dir
 vim.cmd("cd ~")
