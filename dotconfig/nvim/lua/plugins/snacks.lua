17946e05234383d654dc067b377e57720a3c00e0
Matt Briggs <matt@mattbriggs.net>
2025-02-08 13:59:13 -0500

    use explorer in a more normal way

--- dotconfig/nvim/lua/plugins/snacks.lua
+++ dotconfig/nvim/lua/plugins/snacks.lua
@@ -1,294 +1,264 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "snacks_input",
-  callback = function()
-    vim.schedule(function()
-      vim.cmd("startinsert")
-    end)
-  end
+	pattern = "snacks_input",
+	callback = function()
+		vim.schedule(function()
+			vim.cmd("startinsert")
+		end)
+	end,
 })
 
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    bigfile = {
-      enabled = true,
-    },
-    dashboard = {
-      enabled = true,
-      sections = {
-        { section = "header" },
-        { section = "startup", gap = 1, padding = 1 },
-        -- { section = "projects", padding = 1 },
-      },
-    },
-    input = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    explorer = { replace_netrw = false },
-    picker = {
-      ui_select = true,
-      sources = {
-        explorer = {},
-        aerial = require("mbriggs.picker-aerial"),
-        directory = require("mbriggs.picker-directory"),
-      },
-      win = {
-        input = {
-          keys = {
-            ["<Esc>"] = { "close", mode = { "n", "i" } },
-          },
-        },
-      },
-    },
-  },
-  keys = {
-    --- Picker
-    {
-      "<leader>,",
-      function()
-        Snacks.picker.buffers()
-      end,
-      desc = "Buffers",
-    },
-    {
-      "<leader>e",
-      function()
-        Snacks.picker.explorer({
-          finder = "explorer",
-          sort = { fields = { "sort" } },
-          tree = true,
-          supports_live = true,
-          follow_file = true,
-          focus = "list",
-          auto_close = false,
-          jump = { close = false },
-          layout = { preset = "sidebar", preview = false },
-          formatters = { file = { filename_only = true } },
-          matcher = { sort_empty = true },
-          config = function(opts)
-            return require("snacks.picker.source.explorer").setup(opts)
-          end,
-          win = {
-            list = {
-              keys = {
-                ["-"] = "explorer_up",
-                ["%"] = "explorer_add",
-                ["d"] = "explorer_del",
-                ["r"] = "explorer_rename",
-                ["c"] = "explorer_copy",
-                ["m"] = "explorer_move",
-                ["y"] = "explorer_yank",
-                ["<c-c>"] = "explorer_cd",
-                ["."] = "explorer_focus",
-              },
-            },
-          },
-        })
-      end,
-      desc = "Explorer",
-    },
-    {
-      "<leader>/",
-      function()
-        Snacks.picker.grep()
-      end,
-      desc = "Grep",
-    },
-    {
-      "<leader>:",
-      function()
-        Snacks.picker.command_history()
-      end,
-      desc = "Command History",
-    },
-    {
-      "<leader>;",
-      function()
-        Snacks.picker.files()
-      end,
-      desc = "Find Files",
-    },
-    {
-      "<leader><tab>",
-      function()
-        Snacks.picker.resume()
-      end,
-      desc = "Resume Last Pick",
-    },
-    {
-      "<leader>=",
-      function()
-        Snacks.picker.pick("aerial")
-      end,
-      desc = "Find Symbols (Aerial)",
-    },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = {
+			enabled = true,
+		},
+		dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header" },
+				{ section = "startup", gap = 1, padding = 1 },
+				-- { section = "projects", padding = 1 },
+			},
+		},
+		input = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		explorer = { replace_netrw = false },
+		picker = {
+			ui_select = true,
+			sources = {
+				explorer = {},
+				aerial = require("mbriggs.picker-aerial"),
+				directory = require("mbriggs.picker-directory"),
+			},
+			win = {
+				input = {
+					keys = {
+						["<Esc>"] = { "close", mode = { "n", "i" } },
+					},
+				},
+			},
+		},
+	},
+	keys = {
+		--- Picker
+		{
+			"<leader>,",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>e",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "Explorer",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>:",
+			function()
+				Snacks.picker.command_history()
+			end,
+			desc = "Command History",
+		},
+		{
+			"<leader>;",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader><tab>",
+			function()
+				Snacks.picker.resume()
+			end,
+			desc = "Resume Last Pick",
+		},
+		{
+			"<leader>=",
+			function()
+				Snacks.picker.pick("aerial")
+			end,
+			desc = "Find Symbols (Aerial)",
+		},
 
-    -- find
-    {
-      "<leader>fn",
-      function()
-        Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
-      end,
-      desc = "Find Config File",
-    },
-    {
-      "<leader>fj",
-      function()
-        Snacks.picker.jumps()
-      end,
-      desc = "Jumps",
-    },
-    {
-      "<leader>fd",
-      function()
-        Snacks.picker.pick("directory")
-      end,
-      desc = "Directory",
-    },
-    {
-      "<leader>fc",
-      function()
-        Snacks.picker.command_history()
-      end,
-      desc = "Command History",
-    },
-    {
-      "<leader>fC",
-      function()
-        Snacks.picker.commands()
-      end,
-      desc = "Commands",
-    },
-    {
-      '<leader>f"',
-      function()
-        Snacks.picker.registers()
-      end,
-      desc = "Registers",
-    },
-    {
-      "<leader>fh",
-      function()
-        Snacks.picker.help()
-      end,
-      desc = "Help Pages",
-    },
-    {
-      "<leader>fk",
-      function()
-        Snacks.picker.keymaps()
-      end,
-      desc = "Keymaps",
-    },
-    {
-      "<leader>fp",
-      function()
-        Snacks.picker.projects()
-      end,
-      desc = "Projects",
-    },
+		-- find
+		{
+			"<leader>fn",
+			function()
+				Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "Find Config File",
+		},
+		{
+			"<leader>fj",
+			function()
+				Snacks.picker.jumps()
+			end,
+			desc = "Jumps",
+		},
+		{
+			"<leader>fd",
+			function()
+				Snacks.picker.pick("directory")
+			end,
+			desc = "Directory",
+		},
+		{
+			"<leader>fc",
+			function()
+				Snacks.picker.command_history()
+			end,
+			desc = "Command History",
+		},
+		{
+			"<leader>fC",
+			function()
+				Snacks.picker.commands()
+			end,
+			desc = "Commands",
+		},
+		{
+			'<leader>f"',
+			function()
+				Snacks.picker.registers()
+			end,
+			desc = "Registers",
+		},
+		{
+			"<leader>fh",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "Help Pages",
+		},
+		{
+			"<leader>fk",
+			function()
+				Snacks.picker.keymaps()
+			end,
+			desc = "Keymaps",
+		},
+		{
+			"<leader>fp",
+			function()
+				Snacks.picker.projects()
+			end,
+			desc = "Projects",
+		},
 
-    -- search
-    {
-      "<leader>sb",
-      function()
-        Snacks.picker.lines()
-      end,
-      desc = "Buffer Lines",
-    },
-    {
-      "<leader>ss",
-      function()
-        Snacks.picker.grep_buffers()
-      end,
-      desc = "Grep Open Buffers",
-    },
-    {
-      "<leader>sw",
-      function()
-        Snacks.picker.grep_word()
-      end,
-      desc = "Visual selection or word",
-      mode = { "n", "x" },
-    },
-    {
-      "<leader>sq",
-      function()
-        Snacks.picker.qflist()
-      end,
-      desc = "Quickfix List",
-    },
+		-- search
+		{
+			"<leader>sb",
+			function()
+				Snacks.picker.lines()
+			end,
+			desc = "Buffer Lines",
+		},
+		{
+			"<leader>ss",
+			function()
+				Snacks.picker.grep_buffers()
+			end,
+			desc = "Grep Open Buffers",
+		},
+		{
+			"<leader>sw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "Visual selection or word",
+			mode = { "n", "x" },
+		},
+		{
+			"<leader>sq",
+			function()
+				Snacks.picker.qflist()
+			end,
+			desc = "Quickfix List",
+		},
 
-    -- diagnostics
-    {
-      "<leader>x;",
-      function()
-        Snacks.picker.diagnostics()
-      end,
-      desc = "Diagnostics",
-    },
-    {
-      "<leader>xst",
-      function()
-        Snacks.picker.todo_comments()
-      end,
-      desc = "Search Todo",
-    },
-    {
-      "<leader>xsT",
-      function()
-        Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
-      end,
-      desc = "Search Todo/Fix/Fixme",
-    },
+		-- diagnostics
+		{
+			"<leader>x;",
+			function()
+				Snacks.picker.diagnostics()
+			end,
+			desc = "Diagnostics",
+		},
+		{
+			"<leader>xst",
+			function()
+				Snacks.picker.todo_comments()
+			end,
+			desc = "Search Todo",
+		},
+		{
+			"<leader>xsT",
+			function()
+				Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+			end,
+			desc = "Search Todo/Fix/Fixme",
+		},
 
-    --- End Picker
-    {
-      "<leader>gb",
-      [[<cmd>lua Snacks.git.blame_line()<cr>]],
-      desc = "Blame",
-    },
-    {
-      "<leader>gb",
-      [[<cmd>lua Snacks.git.blame_line()<cr>]],
-      desc = "Blame",
-    },
-    {
-      "<leader>gg",
-      [[<cmd>lua Snacks.lazygit.open()<cr>]],
-      desc = "LazyGit",
-    },
-    {
-      "<leader>gl",
-      [[<cmd>lua Snacks.lazygit.log()<cr>]],
-      desc = "Git Log",
-    },
-    {
-      "<leader>gf",
-      [[<cmd>lua Snacks.lazygit.log_file()<cr>]],
-      desc = "Git Log (file)",
-    },
-    {
-      "<leader>H",
-      [[<cmd>lua Snacks.notifier.show_history()<cr>]],
-      desc = "Notifier History",
-    },
-    {
-      "<leader>k",
-      [[<cmd>lua Snacks.scratch()<cr>]],
-      desc = "Toggle Scratch Buffer",
-    },
-    {
-      "<leader>K",
-      [[<cmd>lua Snacks.scratch.select()<cr>]],
-      desc = "Select Scratch Buffer",
-    },
-    {
-      "<leader>Z",
-      [[<cmd>lua Snacks.zen()<cr>]],
-      desc = "Zen Mode",
-    },
-  },
+		--- End Picker
+		{
+			"<leader>gb",
+			[[<cmd>lua Snacks.git.blame_line()<cr>]],
+			desc = "Blame",
+		},
+		{
+			"<leader>gb",
+			[[<cmd>lua Snacks.git.blame_line()<cr>]],
+			desc = "Blame",
+		},
+		{
+			"<leader>gg",
+			[[<cmd>lua Snacks.lazygit.open()<cr>]],
+			desc = "LazyGit",
+		},
+		{
+			"<leader>gl",
+			[[<cmd>lua Snacks.lazygit.log()<cr>]],
+			desc = "Git Log",
+		},
+		{
+			"<leader>gf",
+			[[<cmd>lua Snacks.lazygit.log_file()<cr>]],
+			desc = "Git Log (file)",
+		},
+		{
+			"<leader>H",
+			[[<cmd>lua Snacks.notifier.show_history()<cr>]],
+			desc = "Notifier History",
+		},
+		{
+			"<leader>k",
+			[[<cmd>lua Snacks.scratch()<cr>]],
+			desc = "Toggle Scratch Buffer",
+		},
+		{
+			"<leader>K",
+			[[<cmd>lua Snacks.scratch.select()<cr>]],
+			desc = "Select Scratch Buffer",
+		},
+		{
+			"<leader>Z",
+			[[<cmd>lua Snacks.zen()<cr>]],
+			desc = "Zen Mode",
+		},
+	},
 }
