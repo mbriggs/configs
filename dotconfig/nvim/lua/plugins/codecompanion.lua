5f1b92ef4f71b412e1196922f4f168d8270203a4
Matt Briggs <matt@mattbriggs.net>
2025-02-12 08:43:48 -0500

    add gemini to codecompanion

--- dotconfig/nvim/lua/plugins/codecompanion.lua
+++ dotconfig/nvim/lua/plugins/codecompanion.lua
@@ -1,61 +1,68 @@
 return {
-	"olimorris/codecompanion.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
-	opts = {
-		strategies = {
-			chat = {
-				adapter = "anthropic",
-			},
-			inline = {
-				adapter = "anthropic",
-			},
-		},
-		adapters = {
-			openai = function()
-				return require("codecompanion.adapters").extend("openai", {
-					env = {
-						api_key = "cmd:op read op://Private/OpenAI/code-companion --no-newline",
-					},
-					schema = {
-						model = {
-							default = "o3-mini-high",
-						},
-					},
-				})
-			end,
-			anthropic = function()
-				return require("codecompanion.adapters").extend("anthropic", {
-					env = {
-						api_key = "cmd:op read op://Private/Anthropic/credential --no-newline",
-					},
-				})
-			end,
-		},
-	},
-	config = function(_, opts)
-		require("mbriggs.notifier-codecompanion").setup()
-		require("codecompanion").setup(opts)
-	end,
-	keys = {
-		{ "<leader>cc", "<cmd>CodeCompanionChat toggle<cr>", desc = "Code Chat", mode = { "v", "n" } },
-		{
-			"<C-a>",
-			"<cmd>CodeCompanionActions<cr>",
-			silent = true,
-			desc = "Code Companion Actions",
-			mode = { "v", "n" },
-		},
-		{
-			"ga",
-			"<cmd>CodeCompanionChat Add<cr>",
-			mode = { "v", "n" },
-			desc = "Add to chat",
-			noremap = true,
-			silent = true,
-		},
-	},
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "anthropic",
+      },
+      inline = {
+        adapter = "anthropic",
+      },
+    },
+    adapters = {
+      openai = function()
+        return require("codecompanion.adapters").extend("openai", {
+          env = {
+            api_key = "cmd:op read op://Private/OpenAI/code-companion --no-newline",
+          },
+          schema = {
+            model = {
+              default = "o3-mini-high",
+            },
+          },
+        })
+      end,
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+          env = {
+            api_key = "cmd:op read op://Private/Anthropic/credential --no-newline",
+          },
+        })
+      end,
+      gemini = function()
+        return require("codecompanion.adapters").extend("gemini", {
+          env = {
+            api_key = "cmd:op read op://Private/Gemini/credential --no-newline",
+          },
+        })
+      end,
+    },
+  },
+  config = function(_, opts)
+    require("mbriggs.notifier-codecompanion").setup()
+    require("codecompanion").setup(opts)
+  end,
+  keys = {
+    { "<leader>cc", "<cmd>CodeCompanionChat toggle<cr>", desc = "Code Chat", mode = { "v", "n" } },
+    {
+      "<C-a>",
+      "<cmd>CodeCompanionActions<cr>",
+      silent = true,
+      desc = "Code Companion Actions",
+      mode = { "v", "n" },
+    },
+    {
+      "ga",
+      "<cmd>CodeCompanionChat Add<cr>",
+      mode = { "v", "n" },
+      desc = "Add to chat",
+      noremap = true,
+      silent = true,
+    },
+  },
 }
