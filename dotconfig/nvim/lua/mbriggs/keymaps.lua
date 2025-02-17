603f472c41e659a9da587ce0f12904cf8e814380
Matt Briggs <matt@mattbriggs.net>
2025-02-17 18:29:40 -0500

    updates

--- dotconfig/nvim/lua/mbriggs/keymaps.lua
+++ dotconfig/nvim/lua/mbriggs/keymaps.lua
@@ -4,46 +4,53 @@ local map = vim.keymap.set
 
 map("n", "<leader>#", "<cmd>set nu!<cr>", { desc = "Toggle Line Numbers" })
 map(
-  "n",
-  "<leader>w",
-  ":nohlsearch<CR>:lua require('conform').format()<CR>:w<CR>",
-  { desc = "save->fmt->clear highlights" }
+	"n",
+	"<leader>w",
+	":nohlsearch<CR>:lua require('conform').format()<CR>:w<CR>",
+	{ desc = "save->fmt->clear highlights" }
 )
 
 -- window resizing
 -- Arrow keys with Alt for fine control
-map('n', '<A-Up>', ':resize +2<CR>')
-map('n', '<A-Down>', ':resize -2<CR>')
-map('n', '<A-Left>', ':vertical resize -2<CR>')
-map('n', '<A-Right>', ':vertical resize +2<CR>')
+map("n", "<A-Up>", ":resize +2<CR>")
+map("n", "<A-Down>", ":resize -2<CR>")
+map("n", "<A-Left>", ":vertical resize -2<CR>")
+map("n", "<A-Right>", ":vertical resize +2<CR>")
 
 -- Ctrl+ for larger adjustments
-map('n', '<C-Up>', ':resize +10<CR>')
-map('n', '<C-Down>', ':resize -10<CR>')
-map('n', '<C-Left>', ':vertical resize -10<CR>')
-map('n', '<C-Right>', ':vertical resize +10<CR>')
+map("n", "<C-Up>", ":resize +10<CR>")
+map("n", "<C-Down>", ":resize -10<CR>")
+map("n", "<C-Left>", ":vertical resize -10<CR>")
+map("n", "<C-Right>", ":vertical resize +10<CR>")
 
 -- Map Cmd+F to % (jump to matching bracket)
 map({ "n", "v", "o", "i" }, "<D-f>", "%")
 map("c", "<D-f>", '<C-r>="%"<CR>') -- Command mode for :%s/foo etc.
 
+-- Cmd+hjkl to move between windows in every mode
+map({ "n", "v", "o", "i" }, "<D-h>", "<C-w>h")
+map({ "n", "v", "o", "i" }, "<D-j>", "<C-w>j")
+map({ "n", "v", "o", "i" }, "<D-k>", "<C-w>k")
+map({ "n", "v", "o", "i" }, "<D-l>", "<C-w>l")
+
+map({ "n", "v", "o", "i" }, "<D-f>", "%")
 -- dabbrev style completion
 map("i", "<D-/>", "<C-n>", { desc = "next completion", noremap = "true" })
 map("i", "<D-S-/>", "<C-x><C-n>", { desc = "next completion in current file", noremap = "true" })
 
 -- built in snippets
 map({ "s", "i" }, "<Tab>", function()
-  if vim.snippet.active({ direction = 1 }) then
-    vim.snippet.jump(1)
-  elseif next(vim.lsp.get_clients({ bufnr = 0 })) and vim.lsp.completion then
-    vim.lsp.completion.trigger()
-  end
+	if vim.snippet.active({ direction = 1 }) then
+		vim.snippet.jump(1)
+	elseif next(vim.lsp.get_clients({ bufnr = 0 })) and vim.lsp.completion then
+		vim.lsp.completion.trigger()
+	end
 end)
 
 map("s", "<S-Tab>", function()
-  if vim.snippet.active({ direction = -1 }) then
-    vim.snippet.jump(-1)
-  end
+	if vim.snippet.active({ direction = -1 }) then
+		vim.snippet.jump(-1)
+	end
 end)
 
 -- nuke buffer
@@ -117,13 +124,13 @@ map("c", "%%", "<C-R>=expand('%:h').'/'<cr>", { desc = "put current dir into com
 
 -- maps for diffs
 vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
-  pattern = "*",
-  callback = function()
-    local map = vim.keymap.set
-    if vim.wo.diff then
-      local opts = { buffer = true, noremap = true, silent = true }
-      map("n", "gf", "diffget //2", opts)
-      map("n", "gj", "diffget //3", opts)
-    end
-  end,
+	pattern = "*",
+	callback = function()
+		local map = vim.keymap.set
+		if vim.wo.diff then
+			local opts = { buffer = true, noremap = true, silent = true }
+			map("n", "gf", "diffget //2", opts)
+			map("n", "gj", "diffget //3", opts)
+		end
+	end,
 })
--- dotconfig/nvim/lua/mbriggs/term.lua
+++ dotconfig/nvim/lua/mbriggs/term.lua
@@ -5,25 +5,25 @@ vim.g.terminal_scrollback_buffer_size = 500000
 -- Toggle terminal buffer
 local pre_term_buf
 map("n", "<leader>m", function()
-  local term_buf = vim.fn.bufnr("^\\*term\\*$")
-  local bufname = vim.fn.bufname("%")
+	local term_buf = vim.fn.bufnr("^\\*term\\*$")
+	local bufname = vim.fn.bufname("%")
 
-  if vim.bo.buftype == "terminal" and bufname == "*term*" then
-    if pre_term_buf and vim.fn.bufexists(pre_term_buf) == 1 then
-      vim.cmd("buffer " .. pre_term_buf)
-    else
-      print("No previous buffer found")
-    end
-    return
-  end
+	if vim.bo.buftype == "terminal" and bufname == "*term*" then
+		if pre_term_buf and vim.fn.bufexists(pre_term_buf) == 1 then
+			vim.cmd("buffer " .. pre_term_buf)
+		else
+			print("No previous buffer found")
+		end
+		return
+	end
 
-  pre_term_buf = vim.fn.bufnr("%")
-  if term_buf ~= -1 then
-    vim.cmd("buffer " .. term_buf)
-  else
-    vim.cmd("terminal")
-    vim.cmd("file *term*")
-  end
+	pre_term_buf = vim.fn.bufnr("%")
+	if term_buf ~= -1 then
+		vim.cmd("buffer " .. term_buf)
+	else
+		vim.cmd("terminal")
+		vim.cmd("file *term*")
+	end
 end, { desc = "Toggle terminal buffer" })
 
 -- Only applies in terminal mode
@@ -34,34 +34,35 @@ map("t", "<C-[>", "<C-\\><C-n>", { silent = true })
 
 -- Disable line numbers in terminal
 vim.api.nvim_create_autocmd("TermOpen", {
-  callback = function()
-    -- Get the command that opened the terminal
-    local cmd = vim.bo.channel and vim.fn.jobpid(vim.bo.channel) and
-        vim.split(vim.fn.system(string.format("ps -p %d -o comm=", vim.fn.jobpid(vim.bo.channel))), "\n")[1]
+	callback = function()
+		-- Get the command that opened the terminal
+		local cmd = vim.bo.channel
+			and vim.fn.jobpid(vim.bo.channel)
+			and vim.split(vim.fn.system(string.format("ps -p %d -o comm=", vim.fn.jobpid(vim.bo.channel))), "\n")[1]
 
-    -- Only proceed if it's a regular shell
-    if cmd and (cmd:match("sh$") or cmd:match("bash$") or cmd:match("zsh$")) then
-      vim.opt_local.number = false
-      vim.opt_local.relativenumber = false
-      vim.cmd("startinsert") -- Start in insert mode
+		-- Only proceed if it's a regular shell
+		if cmd and (cmd:match("sh$") or cmd:match("bash$") or cmd:match("zsh$")) then
+			vim.opt_local.number = false
+			vim.opt_local.relativenumber = false
+			vim.cmd("startinsert") -- Start in insert mode
 
-      map({ "t", "n" }, "<D-,>", function()
-        vim.cmd("stopinsert")
-        vim.cmd("TermRename")
-      end, { buffer = true })
-    end
-  end,
+			map({ "t", "n" }, "<D-,>", function()
+				vim.cmd("stopinsert")
+				vim.cmd("TermRename")
+			end, { buffer = true })
+		end
+	end,
 })
 
 vim.api.nvim_create_user_command("TermRename", function()
-  vim.ui.input({
-    prompt = "Terminal name: ",
-  }, function(name)
-    if name then
-      -- Get current buffer number
-      local bufnr = vim.api.nvim_get_current_buf()
-      -- Set the buffer name
-      vim.api.nvim_buf_set_name(bufnr, name)
-    end
-  end)
+	vim.ui.input({
+		prompt = "Terminal name: ",
+	}, function(name)
+		if name then
+			-- Get current buffer number
+			local bufnr = vim.api.nvim_get_current_buf()
+			-- Set the buffer name
+			vim.api.nvim_buf_set_name(bufnr, name)
+		end
+	end)
 end, {})
