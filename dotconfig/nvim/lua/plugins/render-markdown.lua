0d7c7f73fb82e80d9dbc395532c527456da72264
Matt Briggs <matt@mattbriggs.net>
2025-02-28 08:27:18 -0500

    use render-markdown

---
+++ dotconfig/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,13 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	ft = { "markdown", "codecompanion" },
+	dependencies = {
+		{ "nvim-treesitter" },
+		{ "nvim-tree/nvim-web-devicons" },
+	},
+	opts = {
+		heading = {
+			icons = {},
+		},
+	},
+}
