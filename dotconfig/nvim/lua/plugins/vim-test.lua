0fde0bc6817b6832f5c12004aa9fdd22d3fd6fec
Matt Briggs <matt@mattbriggs.net>
2025-03-02 12:43:17 -0500

    dial, and some small vim config mods

--- dotconfig/nvim/lua/plugins/vim-test.lua
+++ dotconfig/nvim/lua/plugins/vim-test.lua
@@ -10,7 +10,7 @@ return {
 		{ "<leader>tt", "<cmd>TestNearest<cr>", desc = "Test Nearest" },
 		{ "<leader>tf", "<cmd>TestFile<cr>", desc = "Test Current File" },
 		{ "<leader>ta", "<cmd>TestSuite<cr>", desc = "Test Whole Suite" },
-		{ "<leader>tl", "<cmd>TestLast<cr>", desc = "Rerun last test" },
+		{ "<leader>t;", "<cmd>TestLast<cr>", desc = "Rerun last test" },
 		{ "<leader>tg", "<cmd>TestVisit<cr>", desc = "Visit last test" },
 	},
 }
