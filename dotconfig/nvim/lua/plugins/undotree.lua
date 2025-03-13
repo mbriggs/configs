fee418a0ddcf1a76c66f2455ee5b7938fc4580ba
Matt Briggs <matt@mattbriggs.net>
2025-03-13 19:21:39 -0400

    drop a bunch of neovim stuff i should have never added

--- dotconfig/nvim/lua/plugins/undotree.lua
+++ dotconfig/nvim/lua/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return {
-	"mbbill/undotree",
-	keys = {
-		{ "<leader>U", vim.cmd.UndotreeToggle, desc = "Toggle undotree" },
-	},
+  "mbbill/undotree",
+  keys = {
+    { "<leader>U", vim.cmd.UndotreeToggle, desc = "Toggle undotree" },
+  },
 }
