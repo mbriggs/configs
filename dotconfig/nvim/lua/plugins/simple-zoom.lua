bdd3484c8fec5d754034ef9886f80a58ee103340
Matt Briggs <matt@mattbriggs.net>
2025-03-22 17:12:21 -0400

    simple zoom

---
+++ dotconfig/nvim/lua/plugins/simple-zoom.lua
@@ -0,0 +1,10 @@
+return {
+  "fasterius/simple-zoom.nvim",
+  cmd = "SimpleZoomToggle",
+  opts = {
+    hide_tabline = true,
+  },
+  keys = {
+    { "<C-.>", "<cmd>SimpleZoomToggle<CR>" },
+  }
+}
