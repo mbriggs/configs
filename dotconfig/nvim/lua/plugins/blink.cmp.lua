5a885ed906c89ce07b4ce4addb02a9bba3b6e21a
Matt Briggs <matt@mattbriggs.net>
2025-03-24 08:30:45 -0400

    small updates

--- dotconfig/nvim/lua/plugins/blink.cmp.lua
+++ dotconfig/nvim/lua/plugins/blink.cmp.lua
@@ -2,14 +2,6 @@ return {
   "saghen/blink.cmp",
   lazy = false,
   version = "v0.*",
-  dependencies = {
-    "saghen/blink.compat",
-    version = "*",
-    lazy = true,
-    opts = {
-      impersonate_nvim_cmp = false,
-    },
-  },
   opts = {
     keymap = {
       ["<D-space>"] = { "show" },
@@ -20,47 +12,36 @@ return {
       ["<PageDown>"] = { "scroll_documentation_down" },
       ["<PageUp>"] = { "scroll_documentation_up" },
     },
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+      ghost_text = {
+        enabled = true,
+      },
+    },
     sources = {
       default = {
-        "codecompanion",
         "lazydev",
         "lsp",
         "path",
         "snippets",
         "buffer",
-        -- "avante_commands",
-        -- "avante_mentions",
-        -- "avante_files",
       },
+
       providers = {
-        codecompanion = {
-          name = "CodeCompanion",
-          module = "codecompanion.providers.completion.blink",
-          enabled = true,
-        },
         lazydev = {
           name = "LazyDev",
           module = "lazydev.integrations.blink",
           score_offset = 100,
         },
-        -- avante_commands = {
-        -- 	name = "avante_commands",
-        -- 	module = "blink.compat.source",
-        -- 	score_offset = 90, -- show at a higher priority than lsp
-        -- 	opts = {},
-        -- },
-        -- avante_files = {
-        -- 	name = "avante_commands",
-        -- 	module = "blink.compat.source",
-        -- 	score_offset = 100, -- show at a higher priority than lsp
-        -- 	opts = {},
-        -- },
-        -- avante_mentions = {
-        -- 	name = "avante_mentions",
-        -- 	module = "blink.compat.source",
-        -- 	score_offset = 1000, -- show at a higher priority than lsp
-        -- 	opts = {},
-        -- },
       },
     },
   },
