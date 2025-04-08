4131e34af52207526b3aecd85cfc19c01e3d5dc8
Matt Briggs <matt@mattbriggs.net>
2025-04-08 07:24:08 -0400

    mini.surround, some cleanup

--- dotconfig/nvim/lua/plugins/substitute.lua
+++ dotconfig/nvim/lua/plugins/substitute.lua
@@ -1,11 +1,9 @@
 -- Substitute operator to replace text with whatever is in the unnamed register
 return {
-	"gbprod/substitute.nvim",
-	config = true,
-	keys = {
-		{ "s", "<cmd>lua require('substitute').operator()<cr>", desc = "substitute" },
-		{ "ss", "<cmd>lua require('substitute').line()<cr>", desc = "substitute line" },
-		{ "S", "<cmd>lua require('substitute').eol()<cr>", desc = "substitute to eol" },
-		{ "s", "<cmd>lua require('substitute').visual()<cr>", desc = "substitute", mode = { "v", "x" } },
-	},
+  "gbprod/substitute.nvim",
+  config = true,
+  keys = {
+    { "S", "<cmd>lua require('substitute').operator()<cr>", desc = "substitute" },
+    { "S", "<cmd>lua require('substitute').visual()<cr>",   desc = "substitute", mode = { "v", "x" } },
+  },
 }
