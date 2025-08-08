f19fabf1482f78b14d95f41ceeffec9ab4524953
Matt Briggs <matt@mattbriggs.net>
2025-08-08 17:44:09 -0400

    flesh out stimulus lsp configs

--- dotconfig/nvim/lsp/stimulus.lua
+++ dotconfig/nvim/lsp/stimulus.lua
@@ -15,6 +15,21 @@
 --- ```
 return {
 	cmd = { "stimulus-language-server", "--stdio" },
-	filetypes = { "html", "ruby", "eruby", "blade", "php" },
-	root_markers = { "Gemfile", ".git" },
+	filetypes = { "html", "ruby", "eruby", "blade", "php", "javascript", "typescript" },
+	root_markers = { "package.json", "Gemfile", ".git" },
+	settings = {
+		stimulus = {
+			controllers = {
+				path = {
+					"app/javascript/controllers",
+					"app/assets/javascripts/controllers",
+					"app/frontend/controllers",
+					"app/packs/controllers",
+				},
+				pattern = "**/*_controller.{js,ts}",
+			},
+			importMap = true,
+		},
+	},
 }
+
